07e1326b35ddcceb86e594b16135b84582f2d7a3
dokato <raymon92@gmail.com>
2021-01-14 23:38:07 +0000

    Update grid_cluster.sh

--- grid_cluster.sh
+++ grid_cluster.sh
@@ -6,5 +6,4 @@
 #SBATCH --cpus-per-task 1
 #SBATCH -o logs/wwi_%j.out
 #SBATCH -e logs/wwi_%j.err
-#python grid5_ww.py ${SLURM_ARRAY_TASK_ID} 1000
-python ww2_grid_iter.py gridvalsf/
+python grid_ww_iter.py gridvalsf/
